0539397e1b5af90679538b6b5bf733b067a8984e
Aryan Baranwal <aryanbaranwal131214@gmail.com>
2026-02-26 23:21:27 +0530

    changed color of wheel again

--- src/components/WheelViewer.tsx
+++ src/components/WheelViewer.tsx
@@ -87,7 +87,7 @@ const Wheel = ({ url, position, rotation, scale }: WheelProps) => {
       >
         <meshStandardMaterial 
           ref={materialRef}
-          color="#966c06" 
+          color="#8f6601" 
           roughness={0.65} 
           metalness={0.45} 
           emissive="#331100" 
